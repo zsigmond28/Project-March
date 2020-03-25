28a4db7c47c9974b970a5d1ed5490102e4b51813
Attila Zsigmond <email@email.com>
2020-03-25 10:29:50 +0100

    Alap

---
+++ src/hu/attilazsigmond/customers/Customers.java
@@ -0,0 +1,36 @@
+package hu.attilazsigmond.customers;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Customers {
+
+    public static void main(String[] args) {
+
+        File file = new File("C:/Users/Attila/Idea_Project/a/src/hu/attilazsigmond/ugyfelek.csv");
+        String sor = "";
+        List lista = new ArrayList();
+
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(file));
+
+            while ((sor = br.readLine()) != null) {
+
+                String [] ugyfel = sor.split(",");
+                lista.add(Arrays.asList(ugyfel));
+
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println(lista);
+
+    }
+
+}
